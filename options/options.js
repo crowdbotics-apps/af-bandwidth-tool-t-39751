c19688e0b8fc5bbe6a53e6ca0ad7ba87177dcbe3
Crowdbotics <team@crowdbotics.com>
2023-04-06 15:28:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "af-bandwidth-tool-t-39751",
+  url: "https://af-bandwidth-tool-t-39751.botics.co",
+  api: "https://af-bandwidth-tool-t-39751.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
